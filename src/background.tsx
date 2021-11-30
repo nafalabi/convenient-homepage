ee902b3f2769082d9dce3bf74ffc0c4a3a3f10c1
Nanda Abi Fahmi <nafal.abi@gmail.com>
2021-11-30 14:05:10 +0700

    setup service worker, add periodic task to clear old background images

--- src/background.tsx
+++ src/background.tsx
@@ -1,1 +1,26 @@
+import clearBackground from "./service-worker/clearBackground";
+
+enum AvailableAlarms {
+  CLEAR_BACKGROUND = "clear-background",
+}
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  switch (alarm.name) {
+    case AvailableAlarms.CLEAR_BACKGROUND:
+      clearBackground();
+      break;
+    default:
+      break;
+  }
+});
+
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.alarms.clearAll();
+  // Define periodic task to clear old background images
+  chrome.alarms.create(AvailableAlarms.CLEAR_BACKGROUND, {
+    periodInMinutes: 30,
+  });
+  console.log("installed");
+});
+
 export {};
---
+++ src/service-worker/clearBackground.ts
@@ -0,0 +1,14 @@
+import dexieDB from "../app/storage/dexie/db";
+
+const clearBackground = async () => {
+  const now = Math.floor(Date.now() / 1000);
+  const threshold = 60 * 60 * 24 * 30; // 30 days
+  const ids = await dexieDB.background
+    .where("expireat")
+    .below(now - threshold)
+    .primaryKeys();
+  await dexieDB.background.bulkDelete(ids);
+  // console.info(`Background images cleared, ids: ${JSON.stringify(ids)}`);
+};
+
+export default clearBackground;
