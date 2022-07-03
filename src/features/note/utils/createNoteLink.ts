0dd32c45ba8eed43a9f6fec3387b1a13ffdf863d
Nanda Abi Fahmi <nafal.abi@gmail.com>
2022-07-03 23:55:29 +0700

    fix: correction on createNoteLink utils

--- src/features/note/utils/createNoteLink.ts
+++ src/features/note/utils/createNoteLink.ts
@@ -2,7 +2,7 @@ export const NOTE_BASE_URL = "/index.html#/note";
 
 const createNoteLink = (noteid?: number | string) => {
   let url = NOTE_BASE_URL;
-  if (noteid) url += `?${noteid}`;
+  if (noteid) url += `?noteid=${noteid}`;
   return url;
 };
 
